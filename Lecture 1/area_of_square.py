849106b82feb705293d6b20172c603b9a6417a4a
Sanyam2345 <sanyamgairola111@gmail.com>
2025-01-18 20:15:39 +0530

    py

--- Lecture 1/area_of_square.py
+++
@@ -1,3 +0,0 @@
-side = float(input("Enter side of square: "))
-area = side ** 2
-print("Area of Square is: ", area)
--- Lecture 1/sum_of_two_numbers.py
+++
@@ -1,4 +0,0 @@
-num1 = int(input("Enter number 1: "))
-num2 = int(input("Enter number 2: "))
-sum = num1 + num2
-print("Sum of the numbers is: ", sum)
