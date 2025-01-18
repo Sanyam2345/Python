c7cc14d8de9763e874102e37b86400efbd2021ab
Sanyam2345 <sanyamgairola111@gmail.com>
2025-01-18 21:19:57 +0530

    py

---
+++ Basic Fundamental Of Python/Variables_in_python.py
@@ -0,0 +1,12 @@
+var = 10
+var1 = 20
+var_1 = 30
+# 1var = 10 #error: can't start variable name with number or
+_var = 50
+# for = 60 #error: Keyword can't be used as a variable name
+# var@_ = 80 #error
+print(var)
+print(var1)
+print(var_1)
+print(_var)
+
